698d261c0ef5a2176743748dcca6caf6501ef8de
Ismael Terreno <iterreno@zigatta.com>
2018-09-29 15:19:06 -0300

    Working with Environment Variables for configurations :).

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,5 @@
+const APP_DB_USER = process.env.APP_DB_USER || "";
+const APP_DB_PASS = process.env.APP_DB_PASS || "";
 module.exports = {
-  url: `mongodb://${encodeURIComponent("root-demo")}:${encodeURIComponent("root-demo")}@localhost:27017/square-app?authSource=admin`
+    url: `mongodb+srv://${encodeURIComponent(APP_DB_USER)}:${encodeURIComponent(APP_DB_PASS)}@cluster0-cs0aw.mongodb.net/test?retryWrites=true`
 };
